3d3277dd46572743504c129b4fcd7ce121525e30
fdeitelhoff <fabian@fabiandeitelhoff.de>
2022-12-27 12:22:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fabians-web-perlen-episode-24-vueuse
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fabians-web-perlen-episode-24-vueuse)
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VueUseDemo from './components/VueUseDemo.vue';
+</script>
+
+<template>
+  <VueUseDemo />
+</template>
+
+<style scoped></style>
